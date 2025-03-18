1464b4775e702ff53736be04de1b5d5434ebefc1
jattydaddy <andrewyg625@gmail.com>
2025-03-19 10:29:29 +1300

    Pace of intro messages are now controlled by the player. Defined functions to make text positioning simpler (text now defualts to center of screen).

--- Game.py
+++ Game.py
@@ -47,12 +47,20 @@ inputted_text = ""
 input_active = True
 banned_names = ["yuan", "yuan dong", "yu an", "dong", "yuans dongy", "dongy"]
 
-def denied_input(x, y, text):
+# Define message for when input is denied
+def denied_input(y, text):
     screen.fill(black)
     text = font.render(text, True, white)
-    screen.blit(text, (x, y))
+    text_pos = text.get_rect(center = (screen_width / 2, y))
+    screen.blit(text, (text_pos))
     pygame.display.flip()
     time.sleep(3)
+
+# Define text displayed in center of screen
+def message(y, text):
+    text = font.render(text, True, white)
+    text_pos = text.get_rect(center = (screen_width / 2, y))
+    screen.blit(text, (text_pos))
 
 # Loop to get name of user
 while input_active:
@@ -65,13 +73,13 @@ while input_active:
         elif event.type == pygame.KEYDOWN: # If pygame event is keydown
             if event.key == pygame.K_RETURN: # If return is pressed, check for conditions
                 if len(inputted_text) > 12: # Show message if length of user input is longer than 12
-                    denied_input(default_text_x, default_text_y, "Too long, keep it under 12")
+                    denied_input(default_text_y, "Too long, keep it under 12")
 
                 elif len(inputted_text) <= 0: # Show message if length of user input is same or shorter than 0 somehow
-                    denied_input(default_text_x, 230, "You need a name")
+                    denied_input(default_text_y, "You need a name")
 
                 elif inputted_text.lower() in banned_names: # Show message if name is in banned_names list
-                    denied_input(default_text_x, 230, "Nah you cant play")
+                    denied_input(default_text_y, "Nah you cant play")
 
                 else:
                     input_active = False # End loop
@@ -82,14 +90,15 @@ while input_active:
                 inputted_text += event.unicode # Add typed characters into the variable
 
     screen.fill(black)
+    
 
     # Blit text
-    text = font.render("Please enter your name here", True, white)
-    screen.blit(text, (default_text_x, default_text_y))
+    message(default_text_y, "Welcome to Yuans Love Life")
+    message(default_text_y + text_spacing, "Please enter your name down here")
+    message(default_text_y + text_spacing * 2, "V")
 
     # Show what the user has typed
-    text = font.render(inputted_text, True, white)
-    screen.blit(text, (default_text_x, 280))
+    message(default_text_y + text_spacing * 3, inputted_text)
 
     pygame.display.flip() # Update display
     clock.tick(60) # Set fps
@@ -105,10 +114,7 @@ while input_active:
 background = pygame.image.load("love_background.png")
 background = pygame.transform.scale(background, (screen_width, screen_height))
 
-# Defining intro messages
-def message(x, y, text):
-    text = font.render(text, True, white)
-    screen.blit(text, (x, y))
+message_number = 0
 
 intro_running = True
 start_time = pygame.time.get_ticks() # Get tick at time of loop start
@@ -119,49 +125,51 @@ while intro_running:
         if event.type == pygame.QUIT: # Allows game to be closed
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                message_number += 1
+            elif event.key == pygame.K_LEFT:
+                message_number -= 1
 
-
-    time_elapsed = pygame.time.get_ticks() # Get current running time
-    running_time = time_elapsed - start_time # Make running_time the time the loop has been running
 
     screen.blit(background, (0, 0))
 
-    if running_time > 0 and running_time < 4000:
-        message(default_text_x, default_text_y, f"Alright hi {inputted_text},")
-        message(default_text_x, default_text_y + text_spacing, "Yuan has lost his girlfriend Trista")
-        message(default_text_x, default_text_y + text_spacing * 2, f"You gotta go find her")
+    if message_number == 0:
+        message(default_text_y, f"Alright hi {inputted_text},")
+        message(default_text_y + text_spacing, "Yuan has lost his girlfriend Trista")
+        message(default_text_y + text_spacing * 2, f"You gotta go find her")
 
-    elif running_time > 4000 and running_time < 8000:
+    elif message_number == 1:
 
-        message(default_text_x, default_text_y, "But the only issue is that")
-        message(default_text_x, default_text_y + text_spacing, "the person you find might be Cody,")
-        message(default_text_x, default_text_y + text_spacing * 2, "and Yuan really doesn't want to")
-        message(default_text_x, default_text_y + text_spacing * 3, "date Cody")
+        message(default_text_y, "But the only issue is that")
+        message(default_text_y + text_spacing, "the person you find might be Cody,")
+        message(default_text_y + text_spacing * 2, "and Yuan really doesn't want to")
+        message(default_text_y + text_spacing * 3, "date Cody")
 
-    elif running_time > 8000 and running_time < 12000:
+    elif message_number == 2:
 
-        message(default_text_x, default_text_y, "Yuan so stinky he leaves a")
-        message(default_text_x, default_text_y + text_spacing, "trail of gross slob behind him.")
-        message(default_text_x, default_text_y + text_spacing * 2, "It reveals anyone caught in it,")
-        message(default_text_x, default_text_y + text_spacing * 3, "so use it to find his girlfriend")
-
-
-
-    elif running_time > 12000 and running_time < 16000:
-
-        message(default_text_x, default_text_y, "After the search timer ends, you")
-        message(default_text_x, default_text_y + text_spacing, "will be presented with an option")
-        message(default_text_x, default_text_y + text_spacing * 2, "between Trista or Cody, pick the")
-        message(default_text_x, default_text_y + text_spacing * 3, "person you saw while searching")
+        message(default_text_y, "Yuan so stinky he leaves a")
+        message(default_text_y + text_spacing, "trail of gross slob behind him.")
+        message(default_text_y + text_spacing * 2, "It reveals anyone caught in it,")
+        message(default_text_y + text_spacing * 3, "so use it to find his girlfriend")
 
 
-    elif running_time > 16000 and running_time < 18000:
 
-        message(default_text_x, default_text_y, "(Move with A, D and Spacebar btw)")
+    elif message_number == 3:
+
+        message(default_text_y, "After the search timer ends, you")
+        message(default_text_y + text_spacing, "will be presented with an option")
+        message(default_text_y + text_spacing * 2, "between Trista or Cody, pick the")
+        message(default_text_y + text_spacing * 3, "person you saw while searching")
+
+
+    elif message_number == 4:
+
+        message(default_text_y, "(Move with A, D and Spacebar btw)")
 
 
     
-    elif running_time > 18000:
+    elif message_number == 5:
         intro_running = False
 
     pygame.display.flip() # Update display
@@ -289,10 +297,11 @@ while True:
     button2_y = 250
 
     # Define ending message
-    def end_message(x, y, text):
+    def end_message(text):
         screen.fill(black)
         text = big_font.render(text, True, white)
-        screen.blit(text, (x, y))
+        text_pos = text.get_rect(center = (screen_width / 2, screen_height / 2))
+        screen.blit(text, (text_pos))
         pygame.display.flip()
         time.sleep(3)
 
@@ -324,18 +333,18 @@ while True:
             # Detect mouse click on buttons
             if event.type == pygame.MOUSEBUTTONDOWN and mouse_pos_x >= button_x and mouse_pos_x <= button_x + button_width and mouse_pos_y >= button_y and mouse_pos_y <= button_y + button_height:
                 if selected_person == people_list[0]: # Show win message if correct button is clicked
-                    end_message(180, 300, "Wow gj you win")
+                    end_message("Wow gj you win")
                     button_loop = False
                 else: # Show lose message if wrong button is clicked
-                    end_message(200, 300, "Wow you suck")
+                    end_message("Wow you suck")
                     button_loop = False
 
             if event.type == pygame.MOUSEBUTTONDOWN and mouse_pos_x >= button2_x and mouse_pos_x <= button2_x + button_width and mouse_pos_y >= button2_y and mouse_pos_y <= button2_y + button_height:
                 if selected_person == people_list[1]:
-                    end_message(180, 300, "Wow gj you win")
+                    end_message("Wow gj you win")
                     button_loop = False
                 else:
-                    end_message(200, 300, "Wow you suck")
+                    end_message("Wow you suck")
                     button_loop = False
 
         pygame.display.flip()
@@ -362,7 +371,8 @@ while True:
         screen.fill(black)
 
         text = big_font.render("Click to play again", True, white)
-        screen.blit(text, (150, 300))
+        text_pos = text.get_rect(center = (screen_width / 2, screen_height / 2))
+        screen.blit(text, (text_pos))
 
         pygame.display.flip()
         clock.tick(60)
